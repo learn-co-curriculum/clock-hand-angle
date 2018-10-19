c3043b966e9b9cc35b817fbfc3ee6c44ebf23bef
bethwilson07 <beth.ny.wlsn@gmail.com>
2018-10-19 10:28:11 -0400

    done

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,3 +1,12 @@
 def clock_angle(time)
-    # code your solution here
+  hours, minutes = time.split(":").map {|n| n.to_i}
+  min_angle = minutes*(360/60)
+  hr_angle = hours*(360/12) + minutes*0.5
+  ang_between = abs(hr_angle - min_angle)
+  if ang_between > 180
+    ang_between -= 180
+  end
+  ang_between
+ end
 end
+#pauls solution
